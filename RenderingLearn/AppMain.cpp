867ad7890ebcb5145c1317c8575b4d027b5ddd86
DNoki <490889409@qq.com>
2021-10-18 10:14:31 +0900

    创建项目

---
+++ RenderingLearn/AppMain.cpp
@@ -0,0 +1,15 @@
+#include "AppConfig.h"
+#include "AppMain.h"
+
+#include <Windows.h>
+#include <shellapi.h>
+#include <stdio.h>
+#include <tchar.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
---
+++ RenderingLearn/AppMain.h
@@ -0,0 +1,3 @@
+﻿#pragma once
+
+extern int main(int argc, char* argv[]);
