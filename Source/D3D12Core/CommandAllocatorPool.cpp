71beb3eb931b06a0edbcb734564038cc8f094a55
DNoki <490889409@qq.com>
2021-11-01 09:22:08 +0900

    工作提交

---
+++ Source/D3D12Core/CommandAllocatorPool.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+
+
+
+#include "CommandAllocatorPool.h"
+
+
+class CommandAllocatorPoolImpl
+{
+public:
+	CommandAllocatorPoolImpl()
+	{
+
+	}
+
+	ID3D12CommandAllocator* RequestAllocator(D3D12_COMMAND_LIST_TYPE Type)
+	{
+		if (m_GraphicsCommandAllocators.empty())
+		{
+
+		}
+	}
+
+private:
+	std::queue<ID3D12CommandAllocator> m_GraphicsCommandAllocators;
+};
--- Source/D3D12Core/pch.h
+++ Source/D3D12Core/pch.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include <functional>
+#include <queue>
 //#include <iostream>
 //#include <string>
 //#include <sstream>
